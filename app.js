53110c3f8eff7971f110a1bddeb42052bf64fcaf
Stefan9731 <57329004+Stefan9731@users.noreply.github.com>
2020-04-02 18:25:43 -0400

    Activity

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+const handlebars = require('express-handlebars');
 const express = require('express');
+const handlebars_inst = handlebars.create({
+    extname: '.handlebars',
+    compilerOptions: {
+        prevenIndent: true
+    },
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials')
+});
 const path = require('path');
 const conf = require('conf');
+app.engine('handlebars', handlebars_inst.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views', 'pages'));
 // create our express app
 const app = express();
 // create our data store for user information
@@ -10,13 +22,15 @@ app.use(express.urlencoded({
  extended: false
 }));
 app.route('/login')
-.get((req, res) => {
-res.sendFile(path.join(__dirname, 'login.html'));
-})
+    get((req, res) => {
+        res.render('login', {
+            field: 'value'
+        })
+    })
 .post((req, res) => {
 // some debug info
 console.log(req.body);
-// TODO implement me
+document.getElementByID("login").submit();
 });
 app.listen(3000, () => {
 console.log('express app running at http://localhost:3000/')
