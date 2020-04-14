1a07627150d586fb372f8cc88441dff794280fb0
Stefan9731 <57329004+Stefan9731@users.noreply.github.com>
2020-04-14 16:49:31 -0400

    dsd

--- app.js
+++ app.js
@@ -13,9 +13,6 @@ const users = {
     password: {
         type: 'string'
     },
-    verified_password: {
-        type: 'string'
-    },
     phone: {
         type: 'number'
     }
@@ -37,6 +34,8 @@ app.set('views', path.join(__dirname, 'views', 'pages'));
 
 // create our data store for user information
 const data = new conf(users);
+data.clear();
+data.set('email', 'test')
 data.set('username', 'test')
 data.set('password', 'test')
 app.use(express.json());
@@ -45,22 +44,67 @@ app.use(express.urlencoded({
 }));
 app.route('/new')
     .get((req,res) =>{
-        res.render('new')
+        res.sendFile(path.join(__dirname, 'new.html'));
     })
-    app.route('/user')
+    .post((req, res) => {
+       console.log(req.body); 
+
+       const user = data.get(req.body.email);
+
+       //Check that passwords match
+       if (req.body.password !== req.body.verified_password) {
+        res.json({
+            message: 'passwords do not match'
+        })
+    }
+       //Check if user already exists
+       else if (user !== undefined){
+           res.json({
+               message: 'User already exisits'
+           })
+       }
+
+       //send success
+       else {
+           //add user to store
+           data.set(req.body.email)({
+               email: req.body.email,
+               password: req.body.password
+           })
+           res.json({
+               message: 'success!'
+           })
+       }
+    })
+
+
+app.route('/user')
     .get((req,res) =>{
         res.render('user')
     })
 app.route('/login')
     .get((req, res) => {
-        res.render('login')
+        res.status(200).sendFile(path.join(__dirname, 'login.html'));
     })
+    .post((req, res) => {
+        const user = data.get(req.body.email);
+        console.log(user);
+        //check that user exists
+        if (user === undefined || user.password !== req.body.password) {
+            res.json({
+                message: 'email/password pair not found'
+            })
+        }
+        //User exists
+        else res.json({
+            message: 'success!'
+        })
+    });
 
 app.route('/user/:user_id')
     .get((req, res) => {
         res.send(req.params.user_id);
     })
-
 app.post((req, res) => {
     res.send('post request')
 // some debug info
